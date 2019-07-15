e38e83fd7c73d039fec00903db7829d8843c7f25
tbiaglow <tbiaglow@gmail.com>
2019-07-15 13:15:54 -0400

    initialized bamazonManager

---
+++ bamazonManager.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "V@der1977",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
