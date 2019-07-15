9a9e06a95ca260081ce7c15889b6be9f04b2c7cf
tbiaglow <tbiaglow@gmail.com>
2019-07-15 03:25:52 -0400

    Initialized inquirer, prompted for item/quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,13 +18,39 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    displayItems();
+    // displayItems();
+    promptPurchase();
 });
 
-function displayItems() {
+function promptPurchase() {
     connection.query("SELECT item_id,product_name,price FROM products", function(err, res) {
         if (err) throw err;
         console.log(res);
-        connection.end;
+        // connection.end;
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "Which item would you like to purchase?"
+            },
+            {
+                type: "input",
+                name: "number",
+                message: "How many would you like to purchase?"
+            }
+        ]).then(function(user) {
+            console.log(res[0].stock_quantity)
+            for (i = 0; i < res.length; i++) {
+                if (res[i].item_id === user.id) {
+                    if (user.number > res[i].stock_quantity) {
+                        console.log("Insufficient quantity!");
+                    }
+                    else if (user.number <= res[i].stock_quantity) {
+                        console.log("Sufficient quantity!")
+                    };
+                }
+            }
+        })
+    connection.end;
     })
 }
