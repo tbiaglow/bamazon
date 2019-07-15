a9bc0ba22f77752fafdb8f5341c07124714048ec
tbiaglow <tbiaglow@gmail.com>
2019-07-15 02:43:31 -0400

    added displayItems function, npm installed mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,21 +1,29 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "V@der1977",
-  database: "bamazon"
+    // Your password
+    password: "V@der1977",
+    database: "bamazon"
 });
 
 connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    displayItems();
 });
+
+function displayItems() {
+    connection.query("SELECT item_id,product_name,price FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end;
+    })
+}
