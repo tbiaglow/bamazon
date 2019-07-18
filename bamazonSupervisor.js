897bb6812ae4a2e914125ee4aac86d8f5c6f1046
tbiaglow <tbiaglow@gmail.com>
2019-07-18 18:14:34 -0400

    Add New Department function created

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -20,5 +20,42 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     // displayItems();
-    promptPurchase();
+    giveOptions();
 });
+
+function giveOptions() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: ["View Product Sales by Department", "Add New Department"]
+        }
+    ]).then(function(choice) {
+        if (choice.choice === "View Product Sales by Department") {}
+        else if (choice.choice === "Add New Department") {
+            connection.query("SELECT * FROM departments", function(err, res){
+                if (err) throw err;
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "departmentName",
+                        message: "What is the name of the new department?"
+                    },
+                    {
+                        type: "input",
+                        name: "overhead",
+                        message: "What are the expected overhead costs of the department?"
+                    }
+                ]).then(function(newProduct) {
+                    var query = connection.query("INSERT INTO departments SET ?", {
+                        department_id: res.length + 1,
+                        department_name: newProduct.departmentName,
+                        over_head_costs: newProduct.overhead
+                    })
+                    console.log("Department created!");
+                })
+            })
+        }
+    })
+}
